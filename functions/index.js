d59c8287297f653b5f7e9b9f1e7fd6492e6daf7f
Fernando Giovani Aldana Ramos <zuvaro010@gmail.com>
2019-10-02 23:04:56 -0500

    Codigo 3

--- functions/index.js
+++ functions/index.js
@@ -14,19 +14,19 @@ exports.saludo = functions.https.onRequest(
       // verifica que el parámetro nombre1 recibido del navegador esté correcto.
       if (!request.query.nombre1) {
         // Entra aquí si el nombre1 es null, undefined o ""
-        throw new Error("Falta el nombre 1");
+        throw new Error("Falta la calificacion 1");
       } else if (!request.query.nombre2) {
         // Entra aquí si el nombre2 es null, undefined o ""
-        throw new Error("Falta el nombre 2");
+        throw new Error("Falta la calificacion 2");
       }else if (!request.query.nombre3) {
         // Entra aquí si el nombre2 es null, undefined o ""
-        throw new Error("Falta el nombre 3");
+        throw new Error("Falta la calificacion 3");
       }else if (!request.query.nombre4) {
         // Entra aquí si el nombre2 es null, undefined o ""
-        throw new Error("Falta el nombre 4");
+        throw new Error("Falta la calificacion 4");
       }else if (!request.query.nombre5) {
         // Entra aquí si el nombre2 es null, undefined o ""
-        throw new Error("Falta el nombre 5");
+        throw new Error("Falta la calificacion 5");
       }
       /* Solo se llega a esta parte si nombre1 y nombre 2 tienen un texto.
        * Devuelve un saludo. */
